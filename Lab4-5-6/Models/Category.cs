25aa6bbcb220e053a3457290956f21fc6aa692c4
ThienBao <Thienbao11091999@gmail.com>
2020-06-01 11:30:13 +0700

    luu bai cuoi buoi

---
+++ Lab4-5-6/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab4_5_6.Models
+{
+    public class Category
+    {
+        public byte Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
---
+++ Lab4-5-6/ViewModels/FutureDateAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Lab4_5_6.ViewModels
+{
+    internal class FutureDateAttribute : Attribute
+    {
+    }
+}
